397ad4d338f3a493817a8ca27ba4001ef6fe9846
Lygitan <564141246@qq.com>
2016-10-15 12:37:25 +0800

    home。。。

--- js/home.js
+++ js/home.js
@@ -2,5 +2,11 @@
  * Created by lx on 2016/10/13.
  */
 $(function () {
-    
+   // -----------------------------------------------------------banner轮播
+   //  for (var i=0;i<5;i++) {
+        //克隆第一个li 5次并添加到ul去
+        // $('.banner-describe').append($('.banner-describe li').eq(0).clone(true));
+
+        $('.banner-describe li').eq(0).clone(true).appendTo('.banner-describe');
+    // }
 });
